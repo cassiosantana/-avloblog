b4a838bae18dbeba7cee480d64149310225e3265
Cassio Santana <cassioo.roger@gmail.com>
2023-06-01 17:30:59 -0300

    Fix post search

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[body category_id created_at id tittle updated_at user_id]
+  end
+
   def excerpt
     ActionView::Base.full_sanitizer.sanitize(body).truncate 30
   end
