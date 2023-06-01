2e412a7a15922ae606b3560ad41fea3d4822b0bf
Cassio Santana <cassioo.roger@gmail.com>
2023-06-01 16:30:00 -0300

    Add dropdown to select commentable and post

--- app/avo/resources/comment_resource.rb
+++ app/avo/resources/comment_resource.rb
@@ -7,7 +7,8 @@ class CommentResource < Avo::BaseResource
 
   field :id, as: :id
   # Fields generated from the model
-  field :commentable, as: :text
+  field :commentable, as: :belongs_to, polymorphic_as: :commentable, types:
+    [::Post]
   field :body, as: :textarea
   field :user, as: :belongs_to
   # add fields here
