404bd9356777f9141f0e3186c61aae71fc96e2d3
Cassio Santana <cassioo.roger@gmail.com>
2023-06-01 13:15:43 -0300

    Add category post count column

--- app/avo/resources/category_resource.rb
+++ app/avo/resources/category_resource.rb
@@ -9,6 +9,9 @@ class CategoryResource < Avo::BaseResource
   # Fields generated from the model
   field :title, as: :text
   field :description, as: :textarea
+  field :posts_count, as: :text do |model|
+    model.posts.count
+  end
 
   field :posts, as: :has_many, attach_scope: -> { query.where.not(category_id: parent.id)
                                                        .or(query.where(category_id: nil)) }
