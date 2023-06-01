316d9a0ed0b7a046127ce91cd185f7aefe654a8b
Cassio Santana <cassioo.roger@gmail.com>
2023-06-01 11:44:11 -0300

    Add category model

--- app/avo/resources/post_resource.rb
+++ app/avo/resources/post_resource.rb
@@ -1,7 +1,7 @@
 class PostResource < Avo::BaseResource
   self.title = :tittle
   self.includes = []
-  self.search_query = -> (params:)do
+  self.search_query = ->(params:) do
     scope.ransack(id_eq: params[:q], tittle_cont: params[:q], m: "or").result(distinct: false)
   end
 
