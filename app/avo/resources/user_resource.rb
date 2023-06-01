eb9f84a769e4acc0ca957cba11ed16ca19cf9d92
Cassio Santana <cassioo.roger@gmail.com>
2023-06-01 14:03:15 -0300

    Make user searchable

--- app/avo/resources/user_resource.rb
+++ app/avo/resources/user_resource.rb
@@ -1,9 +1,9 @@
 class UserResource < Avo::BaseResource
-  self.title = :id
+  self.title = :name
   self.includes = []
-  # self.search_query = -> do
-  #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
-  # end
+  self.search_query = ->(params:) do
+    scope.ransack(id_eq: params[:q], name_cont: params[:q], email_cont: params[:q], m: "or").result(distinct: false)
+  end
 
   field :id, as: :id
   # Fields generated from the model
