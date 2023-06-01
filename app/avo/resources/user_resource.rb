6abfbb65cac023bf9c96dee8b61863c1d1c33153
Cassio Santana <cassioo.roger@gmail.com>
2023-06-01 17:28:23 -0300

    Remove unnecessary parameter from search_query from user_resource.rb

--- app/avo/resources/user_resource.rb
+++ app/avo/resources/user_resource.rb
@@ -1,7 +1,7 @@
 class UserResource < Avo::BaseResource
   self.title = :name
   self.includes = []
-  self.search_query = ->(params:) do
+  self.search_query = -> do
     scope.ransack(id_eq: params[:q], name_cont: params[:q], email_cont: params[:q], m: "or").result(distinct: false)
   end
 
