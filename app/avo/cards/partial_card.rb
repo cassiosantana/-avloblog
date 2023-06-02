09dd23d96a037ba6db7f2109f200da7e3e12496c
Cassio Santana <cassioo.roger@gmail.com>
2023-06-02 09:22:36 -0300

    Increases the size of use columns and rows in the dashboard in partial_card.rb

--- app/avo/cards/partial_card.rb
+++ app/avo/cards/partial_card.rb
@@ -2,7 +2,7 @@ class PartialCard < Avo::Dashboards::PartialCard
   self.id = "partial_card"
   self.label = "Partial card"
   self.partial = "avo/cards/partial_card"
-  # self.display_header = true
-  # self.cols = 1
-  # self.rows = 1
+  self.display_header = false
+  self.cols = 3
+  self.rows = 3
 end
