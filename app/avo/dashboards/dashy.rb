cbce62abb135d6a760d88bdbab035e57a0159fde
Cassio Santana <cassioo.roger@gmail.com>
2023-06-02 07:29:15 -0300

    Add dashboard description

--- app/avo/dashboards/dashy.rb
+++ app/avo/dashboards/dashy.rb
@@ -1,7 +1,7 @@
 class Dashy < Avo::Dashboards::BaseDashboard
   self.id = "dashy"
   self.name = "Dashy"
-  # self.description = "Tiny dashboard description"
+  self.description = "Tiny dashboard description"
   # self.grid_cols = 3
   # self.visible = -> do
   #   true
