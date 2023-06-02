cc6376cea86cc5be364514eb940c67b56a44d7f4
Cassio Santana <cassioo.roger@gmail.com>
2023-06-02 09:43:44 -0300

    Add scope variable with comment amount to tools_controller.rb

--- app/controllers/avo/tools_controller.rb
+++ app/controllers/avo/tools_controller.rb
@@ -2,5 +2,7 @@ class Avo::ToolsController < Avo::ApplicationController
   def custom_tool
     @page_title = "Custom tool"
     add_breadcrumb "Custom tool"
+
+    @something = Comment.count
   end
 end
