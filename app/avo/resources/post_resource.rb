380157f83372b869088a82260c95dfd58c193daf
Cassio Santana <cassioo.roger@gmail.com>
2023-06-02 14:53:44 -0300

    Testes de edição de menu e pesquisa

--- app/avo/resources/post_resource.rb
+++ app/avo/resources/post_resource.rb
@@ -2,17 +2,38 @@ class PostResource < Avo::BaseResource
   self.title = :tittle
   self.includes = [:category]
   self.search_query = -> do
-    scope.ransack(id_eq: params[:q], tittle_cont: params[:q], m: "or").result(distinct: false)
+    if params[:global]
+      # Perform global search
+      scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+    else
+      # Perform resource search
+      scope.ransack(id_eq: params[:q], tittle_cont: params[:q], m: "or").result(distinct: false)
+    end
   end
+
+  self.search_result_path = -> {
+    # Return any path here. You have access to the search `record` the user clicked on.
+    # avo.resources_post_path record, custom: "yup"
+    "https://google.com"
+  }
 
   field :id, as: :id
   # Fields generated from the model
   field :tittle, as: :text
-  field :excerpt, as: :text, only_on: :index
+  field :complex_tittle, as: :text, hide_on: :all, as_label: true do |model|
+    "[#{model.id}] #{model.tittle}"
+  end
+
+  field :excerpt, as: :text, only_on: :index, as_description: true do |model|
+    ActionView::Base.full_sanitizer.sanitize(model.body).truncate 130
+  rescue
+    ""
+  end
   field :body, as: :trix
   field :created_at, as: :date_time
-
   field :category, as: :belongs_to
   field :user, name: 'Author', as: :belongs_to
   # add fields here
+
+  # self.hide_from_global_search = true
 end
--- app/avo/resources/user_resource.rb
+++ app/avo/resources/user_resource.rb
@@ -5,11 +5,13 @@ class UserResource < Avo::BaseResource
     scope.ransack(id_eq: params[:q], name_cont: params[:q], email_cont: params[:q], m: "or").result(distinct: false)
   end
 
+  self.search_query_help = "- You can search by id, name or email."
+
   field :id, as: :id
   field :email, as: :gravatar, link_to_resource: true, as_avatar: true
   field :name, as: :text
   field :email, as: :text, as_description: false
-  field :something, as: :text, as_description: true do |model|
+  field :something, hide_on: :all, as: :text, as_description: true do |model|
     "Something about this record with id of #{model.id}."
   end
 
