caa322caa0ace5e28fe99f153d1cccaf04feff94
Cassio Santana <cassioo.roger@gmail.com>
2023-06-02 09:09:57 -0300

    Add PartialCard

---
+++ app/avo/cards/partial_card.rb
@@ -0,0 +1,8 @@
+class PartialCard < Avo::Dashboards::PartialCard
+  self.id = "partial_card"
+  self.label = "Partial card"
+  self.partial = "avo/cards/partial_card"
+  # self.display_header = true
+  # self.cols = 1
+  # self.rows = 1
+end
