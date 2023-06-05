3b1c9744468ba95ab1b414c89beb534f80c739bd
Cassio Santana <cassioo.roger@gmail.com>
2023-06-05 16:53:31 -0300

    Adds users filter by activity status

--- app/avo/filters/active_user_filter.rb
+++ app/avo/filters/active_user_filter.rb
@@ -5,10 +5,22 @@ class ActiveUserFilter < Avo::Filters::BooleanFilter
   # end
 
   def apply(request, query, values)
+    return query if values['Active'] && values['Inactive']
+
+    if values['Active']
+      query = query.where(active: true)
+    elsif values['Inactive']
+      query = query.where(active: false)
+    end
+
     query
   end
 
+
   def options
-    {}
+    {
+      "Active": "Active",
+      "Inactive": "Inactive"
+    }
   end
 end
