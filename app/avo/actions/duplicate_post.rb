73a751b3ebd9385646bd8ad42930416d1d1d3cab
Cassio Santana <cassioo.roger@gmail.com>
2023-06-05 12:32:01 -0300

    Set the post duplication action to be visible only in the show view and add redirect for duplicate post

--- app/avo/actions/duplicate_post.rb
+++ app/avo/actions/duplicate_post.rb
@@ -2,15 +2,20 @@ class DuplicatePost < Avo::BaseAction
   self.name = "Duplicate post"
   self.message = "Are you sure you want to duplicate this post?"
 
-  # self.visible = -> do
-  #   true
-  # end
+  self.visible = -> do
+    view == :show
+  end
 
-  def handle(models:, **)
+  def handle(**args)
+    models, fields = args.values_at(:models, :fields)
+    duplicated_post = nil
 
     models.each do |model|
       duplicated_post = model.dup
       duplicated_post.save!
     end
+
+    succeed 'Done!, This is the duplicated post.'
+    redirect_to avo.resources_post_path(duplicated_post)
   end
 end
