6024a512f41cbc21f5ab65692ca8beb006875d6c
Cassio Santana <cassioo.roger@gmail.com>
2023-06-06 17:33:14 -0300

    Returns the apply method to the original from the example doc from the course_country_filter.rb file

--- app/avo/filters/course_country_filter.rb
+++ app/avo/filters/course_country_filter.rb
@@ -2,16 +2,8 @@ class CourseCountryFilter < Avo::Filters::BooleanFilter
   self.name = "Course country filter"
 
   def apply(request, query, values)
-    selected_countries = values.select { |country, selected| selected }.keys
-
-    # Checks if the array of selected countries is empty
-    if selected_countries.empty?
-      # Returns the original query without applying the 'where' clause
-      query
-    else
-      # Apply 'where' clause with selected countries
-      query.where(country: selected_countries)
-    end  end
+    query.where(country: values.select { |country, selected| selected }.keys)
+  end
 
   def options
     Course.countries.map { |country| [country, country] }.to_h
