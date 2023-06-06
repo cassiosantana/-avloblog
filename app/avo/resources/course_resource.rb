c4804bacb53f24598a18115f937c70d5797120ca
Cassio Santana <cassioo.roger@gmail.com>
2023-06-06 16:24:25 -0300

    Add field country to Course resource

--- app/avo/resources/course_resource.rb
+++ app/avo/resources/course_resource.rb
@@ -1,11 +1,12 @@
 class CourseResource < Avo::BaseResource
-  self.title = :id
+  self.title = :country
   self.includes = []
   # self.search_query = -> do
   #   scope.ransack(id_eq: params[:q], m: "or").result(distinct: false)
   # end
 
   field :id, as: :id
+  field :country, as: :text
   # add fields here
 
   filter CourseCountryFilter
