82c32f18d5d9c1c63d659c0cf3a8bb04ba5c4f44
Cassio Santana <cassioo.roger@gmail.com>
2023-06-06 16:25:13 -0300

    Fixes error that when removing tags in the filter, no record was displayed

--- app/avo/filters/course_country_filter.rb
+++ app/avo/filters/course_country_filter.rb
@@ -2,8 +2,16 @@ class CourseCountryFilter < Avo::Filters::BooleanFilter
   self.name = "Course country filter"
 
   def apply(request, query, values)
-    query.where(country: values.select { |country, selected| selected }.keys)
-  end
+    selected_countries = values.select { |country, selected| selected }.keys
+
+    # Checks if the array of selected countries is empty
+    if selected_countries.empty?
+      # Returns the original query without applying the 'where' clause
+      query
+    else
+      # Apply 'where' clause with selected countries
+      query.where(country: selected_countries)
+    end  end
 
   def options
     Course.countries.map { |country| [country, country] }.to_h
